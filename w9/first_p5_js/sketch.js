d51797b9ff7f3e75b5bac3f44fb69b8a06cb4562
dbtjswjd <163017875+dbtjswjd@users.noreply.github.com>
2024-11-04 18:46:55 +0900

    원 채우기

--- w9/first_p5_js/sketch.js
+++ w9/first_p5_js/sketch.js
@@ -3,6 +3,18 @@ function setup() {
 }
 
 function draw() {
-  background(220);
-  circle(mouseX, mouseY, 100);
+  background('#333333');
+  fill('cornflowerblue');
+  for (let column = 0; column < width; column += 40) {
+    for (let row = 0; row < width; row += 40) {
+      let x = 20 + column;
+      let y = 20 + row;
+      let diameter = 30;
+      noStroke();
+      circle(x, y, diameter);
+    }
+  }
+  //n과 m이 동시에 필요할경우 같이 쓰지 말기!!!! 헛갈려잉
+  //n쓰고 F2를 누르면 해당쓰인 이름을 모두 한번에 변경가능
 }
+// vs code에서는 int/ float이아닌 let 이나 var 로 사용한다(var는 요즘에 잘사용하지않김함)
