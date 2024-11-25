ddb8d6988e9d01dcd180748428b54231e0b2f710
dbtjswjd <163017875+dbtjswjd@users.noreply.github.com>
2024-11-25 18:49:41 +0900

    12주차 수업

---
+++ w12/matter.js/matter.js-stack/script3/matter-stack.js
@@ -0,0 +1,79 @@
+const Engine = Matter.Engine,
+  Composites = Matter.Composites,
+  MouseConstraint = Matter.MouseConstraint,
+  Mouse = Matter.Mouse,
+  Composite = Matter.Composite,
+  Bodies = Matter.Bodies;
+
+// create engine
+const engine = Engine.create(),
+  world = engine.world;
+
+// create renderer
+// var render = Render.create({
+//   element: document.querySelector('.matter'),
+//   engine: engine,
+//   options: {
+//     showAngleIndicator: true,
+//   },
+// });
+
+// Render.run(render);
+
+// // create runner
+// var runner = Runner.create();
+// Runner.run(runner, engine);
+
+// add bodies
+var stack = Composites.stack(
+  200,
+  606 - 25.25 - 5 * 40,
+  10,
+  5,
+  0,
+  0,
+  function (x, y) {
+    return Bodies.rectangle(x, y, 40, 40);
+  }
+);
+Composite.add(world, [stack]);
+
+let walls = [
+  Bodies.rectangle(400, 0, 800, 50, { isStatic: true }),
+  Bodies.rectangle(800, 300, 50, 600, { isStatic: true }),
+  Bodies.rectangle(0, 300, 50, 600, { isStatic: true }),
+  Bodies.rectangle(400, 606, 800, 50.5, { isStatic: true }),
+];
+
+Composite.add(world, walls);
+
+// add mouse control
+var mouse = Mouse.create(render.canvas),
+  mouseConstraint = MouseConstraint.create(engine, {
+    mouse: mouse,
+    constraint: {
+      stiffness: 0.2,
+      render: {
+        visible: false,
+      },
+    },
+  });
+
+Composite.add(world, mouseConstraint);
+
+// keep the mouse in sync with rendering
+render.mouse = mouse;
+
+// fit the render viewport to the scene
+Render.lookAt(render, {
+  min: { x: 0, y: 0 },
+  max: { x: 800, y: 600 },
+});
+
+// context for MatterTools.Demo
+//스택의 원본보구 뭐가 삭제되어야 하는지 파악해놓기
+
+function setup() {
+  createCanvas(600, 800);
+  console.log;
+}
--- w12/matter.js/script/matter-example.js
+++ w12/matter.js/script/matter-example.js
@@ -1,32 +1,62 @@
-// module aliases
-var Engine = Matter.Engine,
-  Render = Matter.Render,
-  Runner = Matter.Runner,
-  Bodies = Matter.Bodies,
-  Composite = Matter.Composite;
+// module aliases - 가명 aka같은 느낌
+// var Engine = Matter.Engine, //오래된 틀딱!코드-var
+//   Render = Matter.Render,
+//   Runner = Matter.Runner,
+//   Bodies = Matter.Bodies,
+//   Composite = Matter.Composite;
+
+//짧게 쓰기위한 변수처리 objectdis Destructuring
+const { Engine, Render, Runner, Bodies, Composite } = Matter;
+// matter안에 engin...들이있으니까 .해서 불러오는 것들을 engine.matter이런식으로 입력한다고 짧게 만든것
 
 // create an engine
-var engine = Engine.create();
+// var engine = Engine.create();
+//엔진 생성(물리 시뮬레이션을 위한) - 필수
+const anyEngine = Engine.create();
 
 // create a renderer
-var render = Render.create({
+// var render = Render.create({
+//   element: document.body,
+//   engine: engine,
+// });
+//화면에 그리기위한 랜더러 생성 p5에서 그릴거라면 필수는 X
+const anyRender = Render.create({
   element: document.body,
-  engine: engine,
+  engine: anyEngine,
+  options: { width: 600, height: 800 }, // 창 크기 변경사항
 });
 
 // create two boxes and a ground
-var boxA = Bodies.rectangle(400, 200, 80, 80);
-var boxB = Bodies.rectangle(450, 50, 80, 80);
-var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+// var boxA = Bodies.rectangle(400, 200, 80, 80);
+// var boxB = Bodies.rectangle(450, 50, 80, 80);
+// var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });//istatic물리적으로 못건드리게 하는코드
+
+//월드에 집어 넣을 바디를 생성 두개는 박스, 하나는 같은 박스지만 static처리가 되어서 바닥으로 역할한다.
+let boxA = Bodies.rectangle(400, 200, 80, 80); //중앙 좌표center를 중심으로 제작됨
+let boxB = Bodies.rectangle(450, 50, 80, 80);
+let circle = Bodies.circle(200, 100, 80);
+let ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true }); //istatic물리적으로 못건드리게 하는코드
 
 // add all of the bodies to the world
-Composite.add(engine.world, [boxA, boxB, ground]);
+// Composite.add(engine.world, [boxA, boxB, ground]);
+//월드에서 만든 것들을 모두 집어넣기위해서 컴포짓트를 사용함
+Composite.add(anyEngine.world, [boxA, boxB, ground]);
+Composite.add(anyEngine.world, [circle]);
 
 // run the renderer
-Render.run(render);
+// Render.run(render);
+
+//렌더 모듈에게 만든객체를 랜더로 만들라고 말함
+Render.run(anyRender);
 
 // create runner
-var runner = Runner.create();
+// var runner = Runner.create();
+
+//러너 객체 제작
+const anyRunner = Runner.create();
 
 // run the engine
-Runner.run(runner, engine);
+// Runner.run(runner, engine);
+
+//생성된 러너 모듈 실행하도록
+Runner.run(anyRunner, anyEngine);
